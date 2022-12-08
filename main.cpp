88ce27318229207f94b0e809dc06df3799955ad7
mtokmakova <mary.tokmakova@gmail.com>
2022-12-08 23:32:31 +0300

    the last change 2.1

--- main.cpp
+++ main.cpp
@@ -6,34 +6,36 @@
 #include <string>
 #include <stack>
 
+using namespace std;
+
 int AMOUNT_OF_THREADS_NOW = 0;
 bool IS_FOUND = false;
 
-void get_files_and_catalogs(const std::string &root, std::vector<std::string> &files, std::vector<std::string> &catalogs);
-void add_thread(const std::string &root, const std::string &finding, int max_thread);
-void find_file(const std::string &root, const std::string &finding, int max_thread);
-bool is_path_valid(const std::string &root);
-void convert(std::string &root);
+void get_files_and_catalogs(const string &root, vector<string> &files, vector<string> &catalogs);
+void add_thread(const string &root, const string &finding, int max_thread);
+void find_file(const string &root, const string &finding, int max_thread);
+bool is_path_valid(const string &root);
+void convert(string &root);
 
 int main(int argc, char* argv[]) {
     // Подключаем русский язык для файлов с названием на русском
     setlocale(LC_ALL, "rus");
 
-    std::string root = "C:/";
-    std::string finding;
+    string root = "C:/";
+    string finding;
     int max_thread = 10;
 
     // Допустимое количество параметров
     if (argc == 2 || argc == 4 || argc == 6) {
         finding = argv[1];
         for (int i = 2; i < argc; i += 2) {
-            std::string option = argv[i];
-            std::string parameter = argv[i + 1];
+            string option = argv[i];
+            string parameter = argv[i + 1];
             if (option == "--path") {
                 root = parameter;
                 convert(root);
             }
-            else if (option == "--num_threads") max_thread = std::stoi(parameter);
+            else if (option == "--num_threads") max_thread = stoi(parameter);
             else {
                 fprintf(stderr, "%s %s", "There is now such option", option.c_str());
                 return -1;
@@ -42,7 +44,7 @@ int main(int argc, char* argv[]) {
     }
         // Недопустимое количество параметров
     else {
-        fprintf(stderr, "Not valid. Use on of the following:\nfile_name.exe <path>\nfile_name.exe <file name> --path <path> --num_threads <thread amount>");
+        fprintf(stderr, "Not valid. Use on of the following:\nfile_name.exe <file name>\nfile_name.exe <file name> --path <path> --num_threads <thread amount>");
         return -1;
     }
 
@@ -54,25 +56,25 @@ int main(int argc, char* argv[]) {
     find_file(root, finding, max_thread);
     while (AMOUNT_OF_THREADS_NOW > 0) {
         // К этому моменту не все потоки могут завершиться. Терпеливо ждем их завершения
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        this_thread::sleep_for(chrono::milliseconds(100));
     }
     if (!IS_FOUND) {
         // Если файл не найден, выводим сообщение об этом
-        std::cout << "There is no file with this name" << std::endl;
+        cout << "There is no file with this name" << endl;
     }
 
     return 0;
 }
 
 
-void get_files_and_catalogs(const std::string &root, std::vector<std::string> &files, std::vector<std::string> &catalogs) {
+void get_files_and_catalogs(const string &root, vector<string> &files, vector<string> &catalogs) {
     // Функция, считывающая, какие и есть каталоги и файлы в каталоге root
     WIN32_FIND_DATA f;
 
     HANDLE h = FindFirstFile((root + "*").c_str(), &f);
     if(h != INVALID_HANDLE_VALUE) {
         do {
-            std::string fileName = std::string(f.cFileName);
+            string fileName = string(f.cFileName);
             if (fileName != "." && fileName != "..") {
                 // Данная проверка позволяет отфильтровать файлы от каталогов
                 if(f.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
@@ -87,33 +89,33 @@ void get_files_and_catalogs(const std::string &root, std::vector<std::string> &f
 };
 
 
-void add_thread(const std::string &root, const std::string &finding, int max_thread) {
+void add_thread(const string &root, const string &finding, int max_thread) {
     // Добавление одного треда
     AMOUNT_OF_THREADS_NOW++;
     find_file(root, finding, max_thread);
     AMOUNT_OF_THREADS_NOW--;
 }
 
-void find_file(const std::string &root, const std::string &finding, int max_thread) {
+void find_file(const string &root, const string &finding, int max_thread) {
     // Основная функция для поиска файла
     if (IS_FOUND) return;
-    std::stack<std::string> stack_catalogs;
+    stack<string> stack_catalogs;
     stack_catalogs.push(root);
 
     while (!stack_catalogs.empty()) {
-        std::string temp_root = stack_catalogs.top();
+        string temp_root = stack_catalogs.top();
         stack_catalogs.pop();
 
-        std::vector<std::string> files;
-        std::vector<std::string> catalogs;
+        vector<string> files;
+        vector<string> catalogs;
 
         get_files_and_catalogs(temp_root, files, catalogs);
         // В цикле перебираем файлы. Если нашли - выводим найденный файл и заканчиваем выполнение функции
         for (int i = 0; i < files.size(); i++) {
             if (files[i] == finding && !IS_FOUND) {
                 IS_FOUND = true;
-                std::string result = "Founded file: " + temp_root + files[i] + "\n";
-                std::cout << result;
+                string result = "Founded file: " + temp_root + files[i] + "\n";
+                cout << result;
                 return;
             }
         }
@@ -121,7 +123,7 @@ void find_file(const std::string &root, const std::string &finding, int max_thre
         // Перебираем каталоги. Если есть возможность выделить тред - делаем это. Иначе продолжаем считать в этой же функции
         for (int i = 0; i < catalogs.size(); i++) {
             if (AMOUNT_OF_THREADS_NOW < max_thread) {
-                std::thread thr(add_thread, temp_root + catalogs[i] + "/", finding, max_thread);
+                thread thr(add_thread, temp_root + catalogs[i] + "/", finding, max_thread);
                 thr.detach();
             }
             else {
@@ -131,7 +133,7 @@ void find_file(const std::string &root, const std::string &finding, int max_thre
     }
 }
 
-bool is_path_valid(const std::string &root) {
+bool is_path_valid(const string &root) {
     // Проверка, корректно ли написан путь и есть ли по этому пути папка
     WIN32_FIND_DATA f;
 
@@ -143,7 +145,7 @@ bool is_path_valid(const std::string &root) {
     return true;
 }
 
-void convert(std::string &root) {
+void convert(string &root) {
     // Функция, которая меняет символы '\' в '/'
     for (int i = 0; i < root.size(); i++) {
         if (root[i] == '\\') root[i] = '/';
