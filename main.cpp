9d34b04b31c94b9c4501a4738efd4f811dfd09a7
mtokmakova <mary.tokmakova@gmail.com>
2022-12-21 21:40:52 +0300

    tree search

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include <cstdio>
 #include <string>
 #include <queue>
-
 using namespace std;
 bool IS_FOUND = false;
 
@@ -24,10 +23,13 @@ struct Tree {
     }
 };
 
+int AMOUNT_OF_THREADS_NOW = 0;
+
+void add_thread(Tree &tree, const string &finding, int max_thread);
 void clear_tree(Tree &tree);
 void get_files_and_catalogs(const string &root, vector<string> &files, vector<string> &catalogs);
-void build_tree(const string &root, Tree* tree);
-void find_file(const string &root, const string &finding, int max_thread);
+void build_tree(const string &root, Tree &tree);
+void find_file(Tree &tree, const string &finding, int max_thread);
 bool is_path_valid(const string &root);
 void convert(string &root);
 
@@ -35,18 +37,55 @@ int main(int argc, char* argv[]) {
     // Подключаем русский язык для файлов с названием на русском
     setlocale(LC_ALL, "rus");
 
-//    string root = "C:";
-//    string root = "C:";
-    string root = argv[1];
+    string root = "C:";
+
     string finding;
-    int max_thread = 10;
+    int max_threads = 10;
+
+    //    // Допустимое количество параметров
+    if (argc == 2 || argc == 4 || argc == 6) {
+        finding = argv[1];
+        for (int i = 2; i < argc; i += 2) {
+            string option = argv[i];
+            string parameter = argv[i + 1];
+            if (option == "--path") {
+                root = parameter;
+                convert(root);
+            }
+            else if (option == "--num_threads") max_threads = stoi(parameter);
+            else {
+                fprintf(stderr, "%s %s", "There is now such option", option.c_str());
+                return -1;
+            }
+        }
+    }
+        // Недопустимое количество параметров
+    else {
+        fprintf(stderr, "Not valid. Use one of the following:\nfind_app.exe <file name>\nfind_app.exe <file name> --path <path> --num_threads <thread amount>");
+        return -1;
+    }
+
+    // Если путь некорректен, заканчиваем выполнение программы
+    if (!is_path_valid(root)) {
+        return -1;
+    }
 
     Tree tree;
     tree.full_name = root;
     cout << "Building tree..." << endl;
-    build_tree(root, &tree);
-    cout << tree.full_name << endl;
-    cout << "Tree was built..." << endl;
+    build_tree(root, tree);
+    cout << "Tree is built..." << endl;
+    find_file(tree, finding, max_threads);
+
+    while (AMOUNT_OF_THREADS_NOW > 0) {
+        // К этому моменту не все потоки могут завершиться. Терпеливо ждем их завершения
+        this_thread::sleep_for(chrono::milliseconds(100));
+    }
+
+    if (!IS_FOUND) {
+        // Если файл не найден, выводим сообщение об этом
+        cout << "There is no file with this name" << endl;
+    }
 
 
     clear_tree(tree);
@@ -75,11 +114,12 @@ void get_files_and_catalogs(const string &root, vector<string> &files, vector<st
     }
 };
 
-void build_tree(const string &root, Tree* tree) {
+void build_tree(const string &root, Tree &tree) {
     queue<string> queue_catalogs;
     queue<Tree*> queue_tree;
     queue_catalogs.push(root);
-    queue_tree.push(tree);
+    queue_tree.push(&tree);
+
 
     while (!queue_tree.empty()) {
         string temp_root = queue_catalogs.front();
@@ -94,11 +134,51 @@ void build_tree(const string &root, Tree* tree) {
 
         temp_tree->add_child(files, catalogs);
 
-        for (int i = 0; i < temp_tree->childs.size(); i++) {
-            queue_tree.push(temp_tree->childs[i]);
-            queue_catalogs.push(temp_tree->childs[i]->full_name);
+        for (auto & child : temp_tree->childs) {
+            queue_tree.push(child);
+            queue_catalogs.push(child->full_name);
         }
-        tree = temp_tree;
+    }
+}
+
+void add_thread(Tree &tree, const string &finding, int max_thread) {
+    // Добавление одного треда
+    AMOUNT_OF_THREADS_NOW++;
+    find_file(tree, finding, max_thread);
+    AMOUNT_OF_THREADS_NOW--;
+}
+
+void find_file(Tree &tree, const string &finding, int max_thread) {
+    // Основная функция для поиска файла
+    if (IS_FOUND) return;
+
+    queue<Tree*> queue_tree;
+    queue_tree.push(&tree);
+
+
+    while (!queue_tree.empty()) {
+        Tree* temp_tree = queue_tree.front();
+        queue_tree.pop();
+
+        for (const auto &f : temp_tree->files) {
+            if (f == finding && !IS_FOUND) {
+                IS_FOUND = true;
+                cout << "Founded file: " + temp_tree->full_name + "/" + f << endl;
+                return;
+            }
+        }
+
+        for (auto & child : temp_tree->childs) {
+            if (AMOUNT_OF_THREADS_NOW < max_thread) {
+                thread thr(add_thread, ref(*child), finding, max_thread);
+                thr.detach();
+            }
+            else {
+                queue_tree.push(child);
+            }
+        }
+
+        if (IS_FOUND) return;
     }
 }
 
@@ -107,8 +187,8 @@ bool is_path_valid(const string &root) {
     WIN32_FIND_DATA f;
 
     HANDLE h = FindFirstFile((root + "*").c_str(), &f);
-    if(h == INVALID_HANDLE_VALUE || root[root.size() - 1] != '/') {
-        fprintf(stderr, "Path is not valid. Use for example C:/Program Files/");
+    if(h == INVALID_HANDLE_VALUE || root[root.size() - 1] == '/') {
+        fprintf(stderr, "Path is not valid. Use for example C:/Program Files");
         return false;
     }
     return true;
@@ -122,13 +202,11 @@ void convert(string &root) {
 }
 
 void clear_tree(Tree &tree) {
-    for (int i = 0; i < tree.childs.size(); i++) {
-        clear_tree(*tree.childs[i]);
-        delete tree.childs[i];
+    for (auto & child : tree.childs) {
+        clear_tree(*child);
+        delete child;
     }
 }
-
-
 
 
 
